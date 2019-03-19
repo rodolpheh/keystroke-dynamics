0961859544dba24d99fae303873f790bbdfd788b
remicmacs <rembourgeon@gmail.com>
2019-03-19 15:51:50 +0100

    Modify deploy script

--- data/deploy.sh
+++ data/deploy.sh
@@ -1,7 +1,10 @@
+#! /usr/bin/env bash
+
 python -m venv env
 source env/bin/activate
 pip install --upgrade pip
-pip install jupyter-lab ipywidgets ipympl pandas scikit-learn
+pip install jupyterlab ipywidgets ipympl pandas scikit-learn
+
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 jupyter labextension install jupyter-matplotlib
