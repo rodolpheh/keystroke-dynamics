0f7130e1c8578d23d699df9c06d091c6820e251e
rodolpheh <houdas.rodolphe@gmail.com>
2019-03-25 16:52:00 +0100

    Moving files and making an example

--- softs/model/Model.py
+++ softs/model/Model.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from typing import Dict
 
 # Do the math ! Do the math !
-import pandas as pd
 import numpy as np
 
 # Every scikit-learn import
@@ -14,11 +13,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import make_scorer
 from sklearn.base import BaseEstimator
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.model_selection import GridSearchCV
-
-from Data import Data
-from ResultTable import ResultTable
 
 
 class Model(BaseEstimator):
@@ -27,28 +23,28 @@ class Model(BaseEstimator):
         self.nu = nu
         self.gamma = gamma
 
-
     def makePipeline(self) -> None:
         # Create the scaler and the OneClassSVM
         self._scaler = MinMaxScaler((0, 1))
-        self._classifier = OneClassSVM(kernel="rbf", nu=self.nu, gamma=self.gamma)
+        self._classifier = OneClassSVM(
+            kernel="rbf", nu=self.nu, gamma=self.gamma
+        )
 
         # Create the pipeline
-        self._pipeline = Pipeline([('scaler', self.scaler), ('classifier', self.classifier)])
+        self._pipeline = Pipeline(
+            [('scaler', self.scaler), ('classifier', self.classifier)]
+        )
 
         # Create a scorer function
         self._scorer = make_scorer(self.score, greater_is_better=True)
-
 
     @property
     def scaler(self) -> MinMaxScaler:
         return self._scaler
 
-    
     @property
     def classifier(self) -> OneClassSVM:
         return self._classifier
-
 
     @property
     def pipeline(self) -> Pipeline:
@@ -58,12 +54,7 @@ class Model(BaseEstimator):
             self.makePipeline()
             return self._pipeline
 
-
     def fit(self, X: np.array, y: np.array = None) -> Model:
-
-        # Check data
-        #X, y = check_X_y(X, y)
-
         # Fit the pipeline
         self.pipeline.fit(X)
 
@@ -74,7 +65,6 @@ class Model(BaseEstimator):
 
         return self
 
-
     def predict(self, X: np.array):
         # Check is fit had been called
         check_is_fitted(self, ['X_', 'y_'])
@@ -83,22 +73,23 @@ class Model(BaseEstimator):
 
         return self.pipeline.predict(X)
 
-
     def score(self, X: np.array, y: np.array = None) -> float:
         y = self.predict(X)
         TP = y[y == 1].size
         FN = y[y == -1].size
         return TP / (TP + FN)
 
-    
     @staticmethod
-    def evaluate(pipeline: Pipeline, train: np.array, test: np.array) -> Dict[str, float]:
+    def evaluate(
+        pipeline: Pipeline, train: np.array, test: np.array
+    ) -> Dict[str, float]:
         # Predict on all data
+        y_pred_train = pipeline.predict(train)
         y_pred_test = pipeline.predict(test)
-        
+
         TP = y_pred_test[y_pred_test == 1].size
         FN = y_pred_test[y_pred_test == -1].size
-    
+
         return {
             "TP": TP,
             "FN": FN,
@@ -106,21 +97,22 @@ class Model(BaseEstimator):
             "recall": TP / (TP + FN),
             "clf": pipeline
         }
-    
-    
+
     @staticmethod
-    def report(pipeline: Pipeline, train: np.array, test: np.array, outliers: np.array):
-        
+    def report(
+        pipeline: Pipeline, train: np.array, test: np.array, outliers: np.array
+    ):
+
         # Predict on all data
         y_pred_train = pipeline.predict(train)
         y_pred_test = pipeline.predict(test)
         y_pred_outliers = pipeline.predict(outliers)
-        
+
         TP = y_pred_test[y_pred_test == 1].size
         TN = y_pred_outliers[y_pred_outliers == -1].size
         FP = y_pred_outliers[y_pred_outliers == 1].size
         FN = y_pred_test[y_pred_test == -1].size
-        
+
         return {
             "TP": TP,
             "TN": TN,
@@ -134,9 +126,13 @@ class Model(BaseEstimator):
             "clf": pipeline
         }
 
-    
     @staticmethod
-    def findParameters(pipeline: Pipeline, X: np.array, y: np.array = None, returnModel: bool = True):
+    def findParameters(
+        pipeline: Pipeline,
+        X: np.array,
+        y: np.array = None,
+        returnModel: bool = True
+    ):
         returned = {}
         gs = GridSearchCV(
             estimator=pipeline,
