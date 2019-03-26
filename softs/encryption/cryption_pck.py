4dc77769e899b00cb4ea28ba31389760a3696f92
remicmacs <rembourgeon@gmail.com>
2019-03-26 15:32:26 +0100

    Code review encryption package

--- softs/encryption/cryption_pck.py
+++ softs/encryption/cryption_pck.py
@@ -1,77 +1,88 @@
 import pickle
 import random
-from Crypto.Cipher import AES
 import string
-import struct
+from typing import List
+from Crypto.Cipher import AES
 
-def string_gen():
+
+def string_gen() -> str:
     """Génération alétoire d'une chaine de caractères
 
-    Fonction de génération aléatoire d'une chaine de caractères codée sur 16 octets.
-    Utilisé par défaut comme générateur de clé de chiffrement et d'Initialisation Vector.
+    Fonction de génération aléatoire d'une chaine de caractères
+    codée sur 16 octets.
+    Utilisé par défaut comme générateur de clé de chiffrement
+    et d'Initialisation Vector.
+
+    :returns: chaine de caractères aléatoire sur 16 octets
     """
-    key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
+    key = ''.join(random.SystemRandom().choice(
+        string.ascii_uppercase + string.digits) for _ in range(16))
     return key
 
-def encrypt_parameters():
+
+def encrypt_parameters() -> List[str]:
     """Génération d'un vecteur d'initialisation et d'une clé de chiffrement
 
-    Fonction générant un tableau contenant une clé de chiffrement et un vecteur d'initialisation, nécessaire pour le chiffrement AES.
+    Fonction générant un tableau contenant une clé de chiffrement
+    et un vecteur d'initialisation, nécessaire pour le chiffrement AES.
     Stockage de ces paramètres dans un fichier 'param'.
     """
     key = string_gen()
-    iv = string_gen()
-    aesParam=[iv, key]
-    file_dump(aesParam, 'param')
-    return aesParam
+    init_vector = string_gen()
+    aes = [init_vector, key]
+    return aes
 
-def file_dump(crypt, file_name=''):
-    """Fonction de dump de fichier
 
-    Ecriture de la chaine de caractère chiffré dans un fichier.
-    L'utilisateur renseigne au préalable un nom de fichier dans lequel effectuer l'action.
+def file_dump(encrypted_str: str, file_name: str = 'dump.dump'):
+    """Sauve une chaine chiffré dans un fichier
+
+    Ecriture de la chaine de caractère chiffrée dans un fichier.
+    L'utilisateur renseigne au préalable un nom de fichier dans lequel
+    effectuer l'action.
     Le fichier peut etre existant ou non.
     Sert de manière générale pour ecrire un contenu dans un fichier
     """
-    if file_name =='' :
-        file_name = input('Entrez le nom de votre fichier : ')
-    with open(file_name, 'wb') as file_dump:
-        pickle.dump(crypt, file_dump)
-        print("Saved Successfully")
+    with open(file_name, 'wb') as dump_file:
+        pickle.dump(encrypted_str, dump_file)
         return file_name
 
-def file_read(file_name):
-    """Fonction de lecture d'un fichier
 
-    Le fichier est ouvert pour extraire son contenu sous forme d'une chaine de caractère.
+def file_read(file_name: str):
+    """Extrait une chaine chiffrée d'un fichier
+
+    Le fichier est ouvert pour extraire son contenu sous forme
+    d'une chaine de caractère.
     """
     with open(file_name, 'rb') as saved_dump:
         binary_lines = pickle.load(saved_dump)
     return binary_lines
 
-def aes_gen(iv, key):
-    """Fonction de génération de l'AES
 
-    Fonction prenant en entrée une clé de chiffrement et un vecteur d'initialisation.
-    La fonction retourne en sortie, l'AES.
+def aes_gen(init_vector: str, key: str) -> AES:
+    """Obtenir un nouveau objet AES
+
+    Fonction prenant en entrée une clé de chiffrement
+    et un vecteur d'initialisation.
+    La fonction retourne l'AES initialisé.
     """
-    return AES.new(key, AES.MODE_CBC, iv)
+    return AES.new(key, AES.MODE_CBC, init_vector)
 
-def encryption(message, aes):
-    """Fonction de chiffrement d'une chaine
+
+def encrypt(message: str, aes: AES) -> str:
+    """Chiffre un chaîne
 
     Chiffrement d'une chaine codée sur un multiple de 16 octets.
-    Le chiffrement est effectué à l'aide de l'AES, fourni au préalable. La fonction retourné donc la chaine chiffré.
+    Le chiffrement est effectué à l'aide de l'AES, fourni au préalable.
+    La fonction retourne donc la chaine chiffré.
     """
-    print("Data : "+message)
     return aes.encrypt(message)
 
-def decryption(message, aes):
-    """Fonction de déchiffrement d'une chaine
+
+def decrypt(message: str, aes: AES) -> str:
+    """Déchiffre une chaîne
 
     Déchiffrement d'une chaine codée sur un multiple de 16 octets.
-    Le déchiffrement est effectué à l'aide de l'AES, fourni au préalable. La fonction retourné donc la chaine déchiffré.
+    Le déchiffrement est effectué à l'aide de l'AES, fourni au préalable.
+    La fonction retourné donc la chaine déchiffré.
     """
-    print("Crypted data : ")
-    print(message)
     return aes.decrypt(message)
